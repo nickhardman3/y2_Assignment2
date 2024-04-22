2a7089f2f75702f405bae886fde5c14997163438
Nick Hardman <kp22738@bristol.ac.uk>
2024-04-22 14:24:59 +0100

    ammending inconsistent and incorrent indentations

--- Assignment2.py
+++ Assignment2.py
@@ -6,56 +6,56 @@ class ParticleSystem:
 
 	def __init__(self, num_particles: int, dim: int, dt: float, num_steps: int, L: np.ndarray):
 		self.num_particles = num_particles
-        	self.dim = dim
-        	self.dt = dt
-        	self.num_steps = num_steps
-        	self.L = L
-        
-        	self.pos = np.zeros((num_particles, dim))
-        	self.vel = np.zeros((num_particles, dim))
+		self.dim = dim
+		self.dt = dt
+		self.num_steps = num_steps
+		self.L = L
+
+		self.pos = np.zeros((num_particles, dim))
+		self.vel = np.zeros((num_particles, dim))
 
 	def initialize_particles(self, initial_positions: np.ndarray):
-        	self.pos = initial_positions
+		self.pos = initial_positions
 
 	@staticmethod
-	@nb.njit(parallel=True, fastmath=True) 
+	@nb.njit(parallel=True, fastmath=True)
 	def update_lj(pos: np.ndarray, num_particles: int, dim: int, L: np.ndarray):
-        	forces = np.zeros((num_particles, dim))
-        	for i in nb.prange(num_particles):
-            		for j in range(num_particles):
-                	if i != j:
-                    	rij = pos[j] - pos[i]
-                    	r = np.linalg.norm(rij)
-                    	r_unit = rij / r
-                    	r7 = (1/r)**7
-                    	r13 = (1/r)**13
-                    	forces[i] += 24 * (-2*r13 + r7) * r_unit
-        	return forces
+		forces = np.zeros((num_particles, dim))
+		for i in nb.prange(num_particles):
+			for j in range(num_particles):
+				if i != j:
+					rij = pos[j] - pos[i]
+					r = np.linalg.norm(rij)
+					r_unit = rij / r
+					r7 = (1/r)**7
+					r13 = (1/r)**13
+					forces[i] += 24 * (-2*r13 + r7) * r_unit
+		return forces
 
-    	def v(self, acc: np.ndarray):
-        	return self.vel + (self.dt / 2) * acc
-    
-    	def r(self):
-        	return self.pos + self.vel * self.dt
- 
-    	def reflect(self):
-        	half = self.L / 2
-        	for i in range(self.num_particles):
-            		for d in range(self.dim):
-                		if self.pos[i, d] >= half[d]:
-                    			self.pos[i, d] = self.L[d] - self.pos[i, d]
-                    			self.vel[i, d] = -self.vel[i, d]
-                		elif self.pos[i, d] <= -half[d]:
-                    			self.pos[i, d] = -half[d] - self.pos[i, d]
-                    			self.vel[i, d] = -self.vel[i, d]
+	def v(self, acc: np.ndarray):
+		return self.vel + (self.dt / 2) * acc
+
+	def r(self):
+		return self.pos + self.vel * self.dt
+
+	def reflect(self):
+		half = self.L / 2
+		for i in range(self.num_particles):
+			for d in range(self.dim):
+				if self.pos[i, d] >= half[d]:
+					self.pos[i, d] = self.L[d] - self.pos[i, d]
+					self.vel[i, d] = -self.vel[i, d]
+				elif self.pos[i, d] <= -half[d]:
+					self.pos[i, d] = -half[d] - self.pos[i, d]
+					self.vel[i, d] = -self.vel[i, d]
 		return self.pos, self.vel
 
-    	def simulate(self):
-        	new_pos = np.zeros((self.num_steps, self.num_particles, self.dim))
-        	for i in range(self.num_steps):
-            		new_pos[i] = self.pos
-            		acc = self.update_lj(self.pos, self.num_particles, self.dim, self.L)
-            		self.vel = self.v(acc)
-            		self.pos = self.r()
-            		self.reflect()
-        	return new_pos
+	def simulate(self):
+		new_pos = np.zeros((self.num_steps, self.num_particles, self.dim))
+		for i in range(self.num_steps):
+			new_pos[i] = self.pos
+			acc = self.update_lj(self.pos, self.num_particles, self.dim, self.L)
+			self.vel = self.v(acc)
+			self.pos = self.r()
+			self.reflect()
+		return new_pos
