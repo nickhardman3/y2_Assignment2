27626c83256b5c508a6f80e6be12f4207e9ad23b
Nick Hardman <kp22738@bristol.ac.uk>
2024-04-22 14:02:01 +0100

    added function to initialise particles

--- Assignment2.py
+++ Assignment2.py
@@ -3,12 +3,16 @@ import numba as nb
 import matplotlib.pyplot as plt
 
 class ParticleSystem:
+
 	def __init__(self, num_particles: int, dim: int, dt: float, num_steps: int, L: np.ndarray):
-	self.num_particles = num_particles
-        self.dim = dim
-        self.dt = dt
-        self.num_steps = num_steps
-        self.L = L
+		self.num_particles = num_particles
+        	self.dim = dim
+        	self.dt = dt
+        	self.num_steps = num_steps
+        	self.L = L
         
-        self.pos = np.zeros((num_particles, dim))
-        self.vel = np.zeros((num_particles, dim))
+        	self.pos = np.zeros((num_particles, dim))
+        	self.vel = np.zeros((num_particles, dim))
+
+	def initialize_particles(self, initial_positions: np.ndarray):
+        	self.pos = initial_positions
