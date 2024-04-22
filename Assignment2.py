35d75a50ebaff91f2f0150522194ea94fda304c4
Nick Hardman <kp22738@bristol.ac.uk>
2024-04-22 13:59:30 +0100

    added first class __init__ conditions to ParticleSystem class

--- Assignment2.py
+++ Assignment2.py
@@ -2,4 +2,13 @@ import numpy as np
 import numba as nb
 import matplotlib.pyplot as plt
 
-
+class ParticleSystem:
+	def __init__(self, num_particles: int, dim: int, dt: float, num_steps: int, L: np.ndarray):
+	self.num_particles = num_particles
+        self.dim = dim
+        self.dt = dt
+        self.num_steps = num_steps
+        self.L = L
+        
+        self.pos = np.zeros((num_particles, dim))
+        self.vel = np.zeros((num_particles, dim))
