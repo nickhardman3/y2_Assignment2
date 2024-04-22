b9352b2ed5b2ade8ec91b3cd6ea89fb05e5bad0e
Nick Hardman <kp22738@bristol.ac.uk>
2024-04-22 13:55:53 +0100

    removed first class attempt, as was incorrect

--- Assignment2.py
+++ Assignment2.py
@@ -1,13 +1,5 @@
 import numpy as np
+import numba as nb
+import matplotlib.pyplot as plt
 
-class Nbody:
-	def __init__(self, particles, initp, initv, epsilon, sigma, argonmass):
-		self.particles = particles
-		self.positions = np.array(initp, dtype=np.float64)
-		self.velocities = np.array(initv, dtype=np.float64)
-		self.accelerations = np.zeros((particles, 3), dtype=np.float64)
-		self.epsilon = epsilon
-		self.sigma = sigma
-		self.argonmass = argonmass
-		self.tau = ((self.argonmass * self.sigma**2) / self.epsilon)**0.5
 
